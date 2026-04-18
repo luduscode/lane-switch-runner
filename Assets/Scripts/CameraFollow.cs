945a6c395637cf646aa85af9572c44d482798bc0
Luis <luisdtor@gmail.com>
2026-04-18 14:13:51 -0500

    add skybox and tweak camera view

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public Vector3 offset = new Vector3(0f, 5f, -8f);
+    public Vector3 offset = new Vector3(0f, 2.5f, 10f);
 
     void LateUpdate()
     {
